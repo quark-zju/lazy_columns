fbabe04fcea46cf7c064fb191f4b64d74b806fe4
Jun Wu <quark@lihdd.net>
2015-02-12 11:22:13 +0800

    Only load missing columns on demand

--- lib/lazy_columns/acts_as_lazy_column_loader.rb
+++ lib/lazy_columns/acts_as_lazy_column_loader.rb
@@ -17,15 +17,25 @@ module LazyColumns
       end
 
       def define_lazy_accessors_for(columns)
-        columns.each { |column| define_lazy_accessor_for column }
+        columns.each { |column| define_lazy_accessor_for column, columns }
       end
 
-      def define_lazy_accessor_for(column)
+      def define_lazy_accessor_for(column, lazy_columns)
         define_method column do
           unless has_attribute?(column)
-            changes_before_reload = self.changes.clone
-            self.reload
-            changes_before_reload.each{|attribute_name, values| self.send("#{attribute_name}=", values[1])}
+            missing_columns = lazy_columns.reject { |column_name| has_attribute?(column_name) }
+            fresh_record = self.class.unscoped.select(missing_columns).find(id)
+            fresh_attributes = fresh_record.instance_variable_get('@attributes')
+            if @attributes.is_a?(Hash)
+              # Rails 3: @attributes is a Hash containing only the missing columns
+              @attributes.update(fresh_attributes)
+            else
+              # Rails 4: @attributes is an ActiveRecord::AttributeSet
+              #          @attributes.@attributes is a LazyAttributeHash which implements []=
+              missing_columns.each do |column_name|
+                @attributes.instance_variable_get('@attributes')[column_name] = fresh_attributes[column_name]
+              end
+            end
           end
           read_attribute column
         end
